f2438ae13be29cfb43bf47d9acd34de127810cb6
somxserch <somxserch@gmail.com>
2025-01-02 12:35:25 +0000

    se agrego coffeSoft

--- coffeSoft.js
+++ coffeSoft.js
@@ -1,1 +1,666 @@
-console.log('hello world')
+class Complements {
+
+    constructor(link, div_modulo) {
+        this._link = link;
+        this._div_modulo = div_modulo;
+    }
+
+
+    ObjectMerge(target, source) {
+        // Iterar sobre todas las claves del objeto fuente
+        for (const key in source) {
+            // Verificar si la propiedad es propia del objeto fuente
+            if (source.hasOwnProperty(key)) {
+                // Verificar si el valor es un objeto y si el target tiene la misma propiedad
+                if (typeof source[key] === 'object' && source[key] !== null) {
+                    // Si el target no tiene la propiedad o no es un objeto, inicializarla como un objeto vacío
+                    if (!target[key] || typeof target[key] !== 'object') {
+                        target[key] = {};
+                    }
+                    // Llamada recursiva para combinar sub-objetos
+                    this.ObjectMerge(target[key], source[key]);
+                } else {
+                    // Si no es un objeto, asignar el valor directamente
+                    target[key] = source[key];
+                }
+            }
+        }
+        return target;
+    }
+
+
+    useFetch(options) {
+
+        // Valores predeterminados
+        let defaults = {
+            method: 'POST',
+            data: { opc: 'ls' },
+            url: this._link, // La URL debe ser especificada en las opciones
+            success: () => { } // Función vacía por defecto
+        };
+
+        // Mezclar los valores predeterminados con las opciones proporcionadas
+        let opts = Object.assign({}, defaults, options);
+
+        // Validar que la URL esté definida
+        if (!opts.url) {
+            console.error('URL es obligatoria.');
+            return;
+        }
+
+        // Realizar la petición fetch
+        fetch(opts.url, {
+            method: opts.method,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams(opts.data),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                // Llamar a la función success si se proporciona
+                if (typeof opts.success === 'function') {
+                    opts.success(data);
+                }
+            })
+            .catch((error) => {
+                console.error('Error en la petición:', error);
+            });
+    }
+
+}
+
+class Components extends Complements {
+    constructor(link, div_modulo) {
+        super(link, div_modulo);
+    }
+
+    createfilterBar(options) {
+
+        let defaults = {
+            id: 'idFilterBar',
+            parent: 'filterBar',
+            json: [
+                {
+                    opc: "input-calendar",
+                    id: "iptDate",
+                    tipo: "text",
+                    class: "col-6 col-sm-3",
+                    lbl: "Fecha de movimiento",
+                },
+                {
+                    opc: "btn",
+                    fn: "Buscar()",
+                    color: 'primary',
+                    text: "Buscar",
+                    class: "col-sm-2",
+                },
+            ]
+
+        };
+
+        //  Combinar objetos 
+        let opts = Object.assign(defaults, options);
+        $(`#${opts.parent}`).content_json_form({ data: opts.data, type: '', id: opts.id });
+
+
+
+    }
+
+    createTab(options) {
+        let txt = "";
+
+        var defaults = {
+            data: [],
+            id: "myTab",
+            parent: "tabs",
+        };
+
+        // Carga opciones por defecto
+        var opts = Object.assign({}, defaults, options);
+
+        // Creamos el contenedor
+        var div = $("<div>", {
+            class: " ",
+        });
+
+        var ul = $("<ul>", {
+            class: "nav nav-tabs",
+            id: opts.id,
+        });
+
+        var div_content = $("<div>", {
+            class: "tab-content ",
+        });
+
+        for (const x of opts.data) {
+            let active = "";
+            let tab_active = "";
+            if (x.active) {
+                active = "active";
+                tab_active = "show active";
+            }
+
+            var li = $("<li>", {
+                class: "nav-item",
+            });
+
+            // if(x.fn) 
+
+
+
+            // li.html(`<a class="nav-link ${active}" 
+            //     id="${x.id}-tab"  data-bs-toggle="tab" href="#${x.id}"  onclick="${x.fn}"> ${x.tab}</a>  `);
+            li.append(
+                $('<a>', {
+                    class: "nav-link " + active,
+                    id: x.id + "-tab",
+                    "data-bs-toggle": "tab",
+                    href: "#" + x.id,
+                    onclick: x.fn,
+                    text: x.tab
+                })
+            );
+            var div_tab = $("<div>", {
+                class: "tab-pane fade  mt-2 " + tab_active,
+                id: x.id,
+            });
+
+            if (x.contenedor) {
+                // let div_contenedor = $("<div>", {
+                //     class: "row",
+                // });
+
+                for (const y of x.contenedor) {
+                    var div_cont = $("<div>", {
+                        class: y.class,
+                        id: y.id,
+                    });
+
+                    div_tab.append(div_cont);
+                }
+
+                // div_tab.append(div_contenedor);
+            }
+
+            ul.append(li);
+            div_content.append(div_tab);
+        }
+
+        div.append(ul);
+        div.append(div_content);
+        $(`#${opts.parent}`).html(div);
+    }
+
+    createModalForm(options) {
+        const idFormulario = options.id ? options.id : 'frmModal';
+        const components = options.components
+            ? options.components
+            : $("<form>", { novalidate: true, id: idFormulario, class: "" });
+        let defaults = {
+            id: idFormulario,
+            bootbox: {
+                title: 'Modal example',
+                closeButton: true,
+                message: components,
+            },
+            json: [
+                {
+                    opc: 'input-group',
+                    class: 'col-12',
+                    label: 'Nombre'
+                },
+                {
+                    opc: 'btn-submit',
+                    text: 'Guardar',
+                    class: 'col-12'
+                }
+            ],
+            autovalidation: false,
+            data: { opc: 'sendForm' }
+        };
+        const conf = this.ObjectMerge(defaults, options);
+        let modal = bootbox.dialog(conf.bootbox);
+        $('#' + conf.id).content_json_form({ data: conf.json, type: '' });
+        if (options.beforeSend)
+            options.beforeSend();
+        if (conf.autovalidation) {
+            let options_validation = {
+                tipo: "text",
+                opc: "save-frm",
+            };
+            options_validation = Object.assign(options_validation, conf.data);
+            $("#" + conf.id).validation_form(options_validation, (datos) => {
+                fn_ajax(datos, this._link, '').then((data) => {
+                    if (conf.success)
+                        conf.success(data);
+                    modal.modal('hide');
+                });
+            });
+        } else {
+            return modal;
+        }
+        // return modal;
+    }
+
+    createModal(options) {
+
+        let components = $('<div>');
+
+
+        let defaults = {
+            id: '',
+            bootbox: {
+                title: 'Modal example',
+                closeButton: true,
+                message: ' ',
+            },
+
+            extends: false,
+
+            data: { opc: 'lsModal' }
+        };
+
+        const opts = this.ObjectMerge(defaults, options);
+
+
+
+        fn_ajax(opts.data, this._link, '').then((data) => {
+            let modal = bootbox.dialog(opts.bootbox);
+
+
+            if (opts.success)
+                options.success(data);
+
+            // modal.modal('hide');
+
+
+        });
+
+    }
+
+    
+
+}
+
+class Templates extends Components {
+    constructor(link, div_modulo) {
+        super(link, div_modulo);
+    }
+
+    createLayaout(options = {}) {
+        const defaults = {
+            design: true,
+            content: this._div_modulo,
+            parent: '',
+            clean: false,
+            data: { id: "rptFormat", class: "col-12" },
+        };
+
+        const opts = Object.assign({}, defaults, options);
+        const lineClass = opts.design ? ' block ' : '';
+
+        const div = $("<div>", {
+            class: opts.data.class,
+            id: opts.data.id,
+        });
+
+        const row = opts.data.contenedor ? opts.data.contenedor : opts.data.elements;
+
+        row.forEach(item => {
+            let div_cont;
+
+            switch (item.type) {
+
+                case 'div':
+
+                    div_cont = $("<div>", {
+                        class: (item.class ? item.class : 'row') + ' ' + lineClass,
+                        id: item.id,
+                    });
+
+                    if (item.children) {
+                        item.children.forEach(child => {
+                            child.class = (child.class ? child.class + ' ' : '') + lineClass;
+
+                            if (child.type) {
+
+                                div_cont.append($(`<${child.type}>`, child));
+
+                            } else {
+
+                                div_cont.append($("<div>", child));
+                            }
+
+                        });
+                    }
+
+                    div.append(div_cont);
+
+                    break;
+
+                default:
+
+                    const { type, ...attr } = item;
+
+
+                    div_cont = $("<" + item.type + ">", attr);
+
+                    div.append(div_cont);
+                    break;
+            }
+        });
+
+
+        // aplicar limpieza al contenedor
+
+        if (opts.clean)
+            $("#" + opts.content ? opts.content : opts.parent).empty();
+
+
+        if (!opts.parent) {
+            $("#" + opts.content).html(div);
+        } else {
+            $("#" + opts.parent).html(div);
+        }
+
+    }
+
+    createPlantilla(options) {
+
+        let json_components = {
+            id: "mdlGastos",
+            class: "card-body row m-2",
+
+            contenedor: [
+                {
+                    type: "form",
+                    id: "formGastos",
+                    class: " col-lg-4  block pt-2",
+                    novalidate: true,
+                },
+
+                {
+                    type: "div",
+                    id: "contentGast",
+                    class: "col-lg-8 ",
+                    children: [
+                        { class: 'col-12', id: 'filterGastos' },
+                        { class: 'col-12', id: 'tableGastos' }
+                    ]
+                },
+            ]
+        };
+
+
+        var defaults = { data: json_components, design: true };
+        let opts = Object.assign(defaults, options);
+        this.createLayaout(opts);
+
+    }
+
+    splitLayout(options) {
+        let name = options.id ? options.id : 'splitLayout';
+        // Configuración por defecto
+        let defaults = {
+            id: name,
+            parent: this._div_modulo,
+            className: "flex flex-col w-full h-full p-1",
+
+            filterBar: {
+                id: 'filterBar' + name,
+                class: 'w-full h-1/4  line',
+                text: 'filterBar'
+            },
+
+            container: {
+
+                id: 'container' + name,
+                class: 'flex h-2/4 w-full flex-grow ',
+
+                children: [
+                    { class: 'w-1/2 line', id: 'left' + name, text: 'splitlayout' },
+                    { class: 'w-1/2 line', id: 'right' + name }
+                ],
+
+            },
+
+            footer: {
+                id: 'footer' + name,
+                class: 'w-full h-1/4  line',
+            },
+        };
+
+        // Combina los valores predeterminados con las opciones proporcionadas
+        const opts = this.ObjectMerge(defaults, options);
+
+        // Construye el objeto JSON de componentes
+        let jsonComponents = {
+            id: opts.id,
+            class: opts.className,
+            contenedor: [
+                {
+                    type: 'div',
+                    ...opts.filterBar, // Barra de filtros
+                },
+                {
+                    type: 'div',
+                    ...opts.container, // Contenedor central
+                    children: opts.container.children.map((child) => ({
+                        type: 'div',
+                        ...child, // Mapea cada hijo del contenedor
+                    })),
+                },
+                {
+                    type: 'div',
+                    ...opts.footer, // Pie de página
+                },
+            ],
+        };
+
+        // Crea la plantilla con los datos generados
+        this.createPlantilla({
+            data: jsonComponents,
+            parent: opts.parent,
+            design: false,
+        });
+    }
+
+    primaryLayout(options) {
+        const name = options.id ? options.id : 'primaryLayout';
+
+        let defaults = {
+            id: name,
+            parent: this._div_modulo,
+            class: "d-flex mx-2 my-2 h-100",
+            card: {
+                name: "singleLayout",
+                class: "col-12",
+                filterBar: { class: 'w-full line', id: 'filterBar' + name},
+                container: { class: 'w-full my-2 line', id: 'container' +name}
+            }
+        };
+
+
+        // Mezclar opciones con valores predeterminados
+        const opts = this.ObjectMerge(defaults, options);
+
+
+        this.createPlantilla({
+            data: {
+                id: opts.id,
+                class: opts.class,
+                contenedor: [
+                    {
+                        type: "div",
+                        id: opts.card.name,
+                        class: opts.card.class,
+                        children: [
+                            { type: "div", class: opts.card.filterBar.class, id: opts.card.filterBar.id },
+                            { type: "div", class: opts.card.container.class, id: opts.card.container.id }
+                        ]
+                    }
+                ]
+            }, parent: opts.parent, design: false
+        });
+
+    }
+
+    verticalLinearLayout(options) {
+
+        let defaults = {
+            id: '',
+            parent: this._div_modulo,
+            className: "flex m-2 ",
+
+
+            card: {
+                id: "singleLayout",
+                className: "w-full",
+                filterBar: { className: 'w-full  line', id: 'filterBar' },
+                container: { className: 'w-full my-2 line', id: 'container' },
+                footer: { className: 'w-full my-2 line', id: 'footer' },
+            }
+
+
+        };
+
+
+        const opts = this.ObjectMerge(defaults, options);
+        let jsonComponents = {
+            id: opts.id,
+            class: opts.className,
+            contenedor: [
+                {
+                    type: "div",
+                    id: opts.card.id,
+                    class: opts.card.className,
+                    children: [
+                        { class: opts.card.filterBar.className, id: opts.card.filterBar.id },
+                        { class: opts.card.container.className, id: opts.card.container.id },
+                        { class: opts.card.footer.className, id: opts.card.footer.id },
+                    ],
+                },
+            ],
+        };
+        this.createPlantilla({ data: jsonComponents, parent: opts.parent, design: false });
+    }
+
+    secondaryLayout(components) {
+        let nameComponent = {
+            name: 'container',
+            parent: this._div_modulo,
+            className: 'flex p-2 ',
+            cardtable: {
+                className: 'col-4 line',
+                id: 'containertable',
+                filterBar: { id: 'filterTable', className: 'col-12 mb-2 line' },
+                container: { id: 'listTable', className: 'col-12 line' },
+            },
+            cardform: {
+                className: 'col-8 line',
+                id: 'containertable',
+                filterBar: { id: 'filterTicket', className: 'col-12 mb-2 line' },
+                container: { id: 'containerTicket', className: 'col-12 line' },
+            },
+        };
+
+        let ui = this.ObjectMerge(nameComponent, components);
+
+        let jsonComponents = {
+            id: ui.name,
+            class: ui.className,
+
+            contenedor: [
+                {
+                    type: 'div',
+                    id: ui.cardform.id,
+                    class: ui.cardform.className,
+                    children: [
+                        { class: ui.cardform.filterBar.className, id: ui.cardform.filterBar.id },
+                        { class: ui.cardform.container.className, id: ui.cardform.container.id },
+                    ],
+                },
+                {
+                    type: "div",
+                    id: ui.cardtable.id,
+                    class: ui.cardtable.className,
+                    children: [
+                        { class: ui.cardtable.filterBar.className, id: ui.cardtable.filterBar.id },
+                        { class: ui.cardtable.container.className, id: ui.cardtable.container.id },
+                    ]
+                },
+            ],
+        };
+
+        this.createPlantilla({
+            data: jsonComponents,
+            parent: ui.parent,
+            design: false
+        });
+    }
+
+    tabsLayout(components) {
+        let jsonTabs = [
+            { tab: "tab-1", id: "tab-1", active: true },
+            { tab: "tab-2", id: "tab-2" },
+        ];
+        let defaults = {
+            parent: 'tabsLayout',
+            id: 'tabs',
+            json: jsonTabs
+        };
+
+        let opts = Object.assign(defaults, components);
+        $(`#${opts.parent}`).simple_json_tab({ data: opts.json });
+    }
+}
+
+
+async function useFetch(options = {}) {
+
+    // Valores predeterminados
+    let defaults = {
+
+        method: 'POST',
+        data: { opc: 'ls' },
+        url: '',
+        success: null
+
+    };
+
+    // Mezclar los valores predeterminados con las opciones proporcionadas
+    let opts = Object.assign({}, defaults, options);
+
+    // Validar que la URL esté definida
+    if (!opts.url) {
+        console.error('URL es obligatoria.');
+        return null;
+    }
+
+    try {
+        // Realizar la petición fetch
+        let response = await fetch(opts.url, {
+            method: opts.method,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams(opts.data),
+        });
+
+        // Procesar la respuesta como JSON
+        let data = await response.json();
+
+        // Si se proporciona el método success, lo ejecutamos con los datos obtenidos
+        if (typeof opts.success === 'function') {
+            opts.success(data);
+        }
+
+        // Retornar los datos por si se quieren usar fuera de la función success
+        return data;
+    } catch (error) {
+        console.error('Error en la petición:', error);
+        return null;
+    }
+}
+
