97418860e43c9b60ea6c4e8b808f835deb765d68
SomxD <desarrollo080293@gmail.com>
2025-01-12 05:41:39 -0600

    update navbar

--- admin/index.php
+++ admin/index.php
@@ -7,11 +7,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <!-- icons -->
-    <link rel="stylesheet" href="src/plugin/fontello/css/fontello.css">
+    <link rel="stylesheet" href="../src/plugin/fontello/css/fontello.css">
     <!-- styles -->
     <link rel="stylesheet" href="https://erp-varoch.com/ERP24/src/plugin/bootstrap-5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://erp-varoch.com/ERP24/src/css/navbar.css">
-    <link rel="stylesheet" href="./src/css/style.css">
+    <link rel="stylesheet" href="../src/css/style.css">
     <!--SELECT2-->
     <link rel="stylesheet" href="https://erp-varoch.com/ERP24/src/plugin/select2/bootstrap/select2.min.css">
     <link rel="stylesheet"
@@ -31,10 +31,11 @@
     <title> CoffeSoft 2</title>
 
 </head>
-
+<?php include '../layout/navbar.php';?>
 <body>
     <main>
-
+        <section id="sidebar"></section>
+        
         <div id="main__content">
             <div class="" id="root">
 
@@ -49,8 +50,10 @@
 <script src="https://erp-varoch.com/ERP24/src/plugin/jquery/jquery-3.7.0.min.js"></script>
 <!-- <script src="https://erp-varoch.com/ERP24/src/plugin/bootstrap-5/js/bootstrap.min.js"></script> -->
 <script src="https://erp-varoch.com/DEV/src/plugin/bootstrap-5/js/bootstrap.bundle.js"></script>
+
 <!-- bootbox -->
 <script src="https://erp-varoch.com/DEV/src/plugin/bootbox.min.js"></script>
+
 <!-- data range picker -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
 <script src="https://erp-varoch.com/DEV/src/plugin/daterangepicker/daterangepicker.js"></script>
@@ -58,10 +61,14 @@
 <!-- SELECT2 -->
 <script src="https://erp-varoch.com/ERP24/src/plugin/select2/bootstrap/select2.min.js"></script>
 
+<!-- init -->
+<script src="../src/js/sidebar.js?t=<?php echo time(); ?>"></script>
+
+
+
 <!-- aux js -->
 <script src="../src/js/coffeSoft.js?t=<?php echo time(); ?>"></script>
 <script src="../src/js/plugins.js?t=<?php echo time(); ?>"></script>
-
 <script src="src/js/admin.js"></script>
 
 </html>
---
+++ admin/navbar.php
@@ -0,0 +1,33 @@
+<header class="bg-dia">
+    <section>
+        <span type="button" id="btnSidebar"><i class="icon-menu"></i></span>
+        <!-- <img onclick="location.reload()" class="d-none d-sm-block" src="https://erp-varoch.com/ERP2/src/img/logos/logo_row_wh.png" alt="Grupo Varoch">
+        <img onclick="location.reload()" class="d-block d-sm-none" src="https://erp-varoch.com/ERP2/src/img/logos/logo_icon_wh.png" alt="Grupo Varoch"> -->
+    </section>
+    <nav>
+        <ul class="theme" id="navbar">
+            <!-- <ul id="navbar"> -->
+            <!-- <li class="hide">
+                <i class="icon-sun-inv-1"></i>
+            </li>
+            <li class="hide">
+                <i class="icon-bell"></i>
+            </li>
+            <li class="hide">
+                <i class="icon-mail"></i>
+                <ul>
+                    <div id="mensage">
+                        <li>Hola</li>
+                    </div>
+                </ul>
+            </li> -->
+            <!-- <li id="li_user">
+               
+                <ul>
+                    <li onClick="redireccion('perfil/perfil.php');">
+                   
+                </ul>
+            </li> -->
+        </ul>
+    </nav>
+</header>
