f70e7ce0fcf917336dba55f8811af6ff40ba08ec
SomxD <desarrollo080293@gmail.com>
2025-03-02 11:16:06 -0600

    add components

--- plantilla/src/js/app.js
+++ plantilla/src/js/app.js
@@ -25,6 +25,7 @@ $(async () => {
 class App extends Templates {
     constructor(link, div_modulo) {
         super(link, div_modulo);
+        this.PROJECT_NAME = "Survey";
     }
 
     init() {
@@ -39,11 +40,12 @@ class App extends Templates {
     }
 
     layout() {
-        this.primaryLayout({
-            parent: "root",
-            id: "Primary",
-        });
-        this.historyPay();
+        // this.primaryLayout({
+        //     parent: "root",
+        //     id: "Primary",
+        // });
+        // this.historyPay();
+        this.QuestionLayout();
 
     }
 
@@ -101,9 +103,6 @@ class App extends Templates {
         
         const data = await this.useFetch({ url: api_alpha, data: { opc: "getHistory" } });
 
-
-        console.log(data);
-
         this.createTimeLine({
             parent: "containerPrimary",
             data: [
@@ -116,6 +115,138 @@ class App extends Templates {
             ]
         });
     }
+
+
+    // Components. valores
+    QuestionLayout(options) {
+        let jsonComponents = {
+            id: `content-${this.PROJECT_NAME}`,
+            parent:'root',
+            class: "row px-3 py-2",
+            contenedor: [
+                {
+                    type: "div",
+                    class: "col-12 line",
+                    id: `content-header-${this.PROJECT_NAME}`,
+                },
+                {
+                    type: "div",
+                    class: "col  line",
+                    id: `content-questions-${this.PROJECT_NAME}`,
+                    children: [
+                        { class: "col-12 ", id: "groups" },
+                        { class: "col-12 p-2 ", id: "questions" },
+                    ],
+                },
+                {
+                    type: "div",
+                    class: "col-12 text-end py-4 line",
+                    id: `content-footer-${this.PROJECT_NAME}`,
+                },
+            ],
+        };
+
+        let opts = Object.assign(jsonComponents, options);
+
+        this.createPlantilla({
+            data: jsonComponents,
+            parent: opts.parent,
+            design: false,
+        });
+
+
+        // initials.
+        this.createEndEvaluationButtons();
+        this.groupCard();
+    }
+
+    createEndEvaluationButtons() {
+        let buttons = [
+            {
+                opc: 'btn',
+                icon: "icon-logout",
+                color: "outline-secondary fw-bold",
+                text: "Salir de la evaluación",
+                fn: this.instancia + '.backEvaluation()'
+            },
+            {
+                opc: 'button',
+                color: "primary fw-bold",
+                icon: "icon-check",
+                text: "Terminar evaluación",
+                onClick: () => {
+                    this.init();
+                },
+            },
+        ];
+
+        this.createButtonGroup({
+            data: buttons,
+            class: "justify-content-end gap-2 pt-2",
+            parent: "content-footer-" + this.PROJECT_NAME,
+            size: "sm",
+            cols: "w-25 p-2",
+        });
+    }
+
+    async groupCard() {
+
+        this.createButtonGroup({
+            parent: 'groups',
+            cols: 'w-25 p-3',
+            size: 'sm',
+            class: 'd-flex justify-content-start',
+            onClick: () => { this.initEvaluation() },
+            dataEl: {
+                data: [
+                    { id:1,valor: 'LEONARDO DE JESUS MARTINEZ DE LA CRUZ' },
+                    { id:2,valor: 'SERGIO OSORIO MENDEZ'},
+                    { id:3,valor: 'ROSA ANGELICA PEREZ VELAZQUEZ'},
+                 
+                ]
+            }
+        });
+
+       
+    }
+
+
+    initEvaluation(){
+
+        this.createQuestionnaire({
+            parent: 'questions',
+            data: [
+                {
+                    title: 'TRABAJO EN EQUIPO',
+                    questions: [
+                        { text: '¿Se involucra en las actividades de equipo?', options: [1, 2, 3, 4, 5] },
+                        { text: '¿Aporta ideas y soluciones para resolver desafíos en equipo?', options: [1, 2, 3, 4, 5] },
+                        { text: '¿Se comunica de manera clara con el equipo?', options: [1, 2, 3, 4, 5] }
+                    ]
+                },
+                {
+                    title: 'PROFESIONALISMO',
+                    questions: [
+                        { text: '¿Es una persona capacitada para realizar su trabajo?', options: [1, 2, 3, 4, 5] }
+                    ]
+                },
+                {
+                    title: 'TRABAJO EN EQUIPO',
+                    questions: [
+                        { text: '¿Se involucra en las actividades de equipo?', options: [1, 2, 3, 4, 5] },
+                        { text: '¿Aporta ideas y soluciones para resolver desafíos en equipo?', options: [1, 2, 3, 4, 5] },
+                        { text: '¿Se comunica de manera clara con el equipo?', options: [1, 2, 3, 4, 5] }
+                    ]
+                },
+            ]
+        });
+
+    }
+
+    
+
+
+
 
 
 
