304c441f8955297dd3e799ecadd7e8a2ebd1e243
SomxSerch <desarrollo080293@gmail.com>
2025-06-24 17:05:54 -0600

    add

--- rrhh/src/js/app.js
+++ rrhh/src/js/app.js
@@ -40,8 +40,8 @@ class App extends Templates {
 
     render() {
 
-        this.layout();
-        // this.layoutNewRotation()
+        // this.layout();
+        this.layoutNewRotation()
        
     }
 
